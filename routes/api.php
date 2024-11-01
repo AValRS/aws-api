911bfe2cbc3c25da9cb0b65d48aa28cc8bba9020
Luis Valencia <luis17sept@gmail.com>
2024-11-01 17:27:14 -0600

    Configuraciones generales de lenguaje y rutas api

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+// Rutas para API
+
+use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('api')->group(function () {
+    Route::group(['prefix' => 'alumnos'], function () {
+        Route::post('/', [StudentController::class, 'store']);
+        Route::put('/', [StudentController::class, 'update']);
+        Route::delete('/{id}', [StudentController::class, 'delete']);
+        Route::get('/', [StudentController::class, 'find']);
+        Route::get('/{id}', [StudentController::class, 'get']);
+    });
+    Route::group(['prefix' => 'profesores'], function () {
+        Route::post('/', [ProfessorController::class, 'store']);
+        Route::put('/', [ProfessorController::class, 'update']);
+        Route::delete('/{id}', [ProfessorController::class, 'delete']);
+        Route::get('/', [ProfessorController::class, 'find']);
+        Route::get('/{id}', [ProfessorController::class, 'get']);
+    });
+
+});
